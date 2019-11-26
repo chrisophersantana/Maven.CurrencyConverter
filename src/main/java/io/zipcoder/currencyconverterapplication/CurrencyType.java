17536a4aee28490ed7f2cbe9623acbc1a5e9ba7d
Christopher Santana <chris@zipcoders-MacBook-Pro-6.local>
2019-11-26 17:40:32 -0500

    finished labb

--- src/main/java/io/zipcoder/currencyconverterapplication/CurrencyType.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/CurrencyType.java
@@ -25,6 +25,8 @@ public enum CurrencyType {
     }
 
     public static CurrencyType getTypeOfCurrency(ConvertableCurrency currency) {
-        return null;
+
+
+        return currency.getValue();
     }
 }
